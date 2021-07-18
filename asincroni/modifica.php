b2b3c893e777ff52f8d611fa14318ff4a9223c03
FVettore <FVettore@PC141.cifarelli.loc>
2021-07-18 12:31:37 +0200

    Aggiornato README

--- asincroni/modifica.php
+++ asincroni/modifica.php
@@ -8,9 +8,10 @@ $campoid=$pagina->parametri[2];
 $id=$pagina->parametri[3];
 $campo= urldecode($pagina->parametri[4]);
 $valore= urldecode($pagina->parametri[5]);
-$query="UPDATE $tabella set $campo = ? where $campoid = ?";
+//$query="UPDATE $tabella set $campo = ? where $campoid = ?";
+$query="UPDATE ? set $campo = ? where $campoid = ?";
 $smt=$db->prepare($query);
-$smt->bind_param("si",$valore,$id);
+$smt->bind_param("ssi",$tabella,$valore,$id);
 $smt->execute();
 //check valore corretto: query al contrario- restituisce alla pagina che ha chiamato l'asincrono
 $query="select $campo from auser.$tabella where $campoid=?";
