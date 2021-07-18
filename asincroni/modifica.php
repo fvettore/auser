93e480f624908eac3928489cbef00a780fe3e849
FVettore <FVettore@PC141.cifarelli.loc>
2021-07-18 12:34:13 +0200

    Modifiche asincroni

--- asincroni/modifica.php
+++ asincroni/modifica.php
@@ -3,15 +3,15 @@
  *  modifica record
  */
 
-$tabella=$pagina->parametri[1];
-$campoid=$pagina->parametri[2];
+$tabella=$db->real_escape_string($pagina->parametri[1]);
+$campoid=$db->real_escape_string($pagina->parametri[2]);
 $id=$pagina->parametri[3];
 $campo= urldecode($pagina->parametri[4]);
 $valore= urldecode($pagina->parametri[5]);
 //$query="UPDATE $tabella set $campo = ? where $campoid = ?";
-$query="UPDATE ? set $campo = ? where $campoid = ?";
+$query="UPDATE $tabella set $campo = ? where $campoid = ?";
 $smt=$db->prepare($query);
-$smt->bind_param("ssi",$tabella,$valore,$id);
+$smt->bind_param("si",$valore,$id);
 $smt->execute();
 //check valore corretto: query al contrario- restituisce alla pagina che ha chiamato l'asincrono
 $query="select $campo from auser.$tabella where $campoid=?";
